ceea62ae25303c198528dbbc8e284166bac703a6
jeongjoonsup <penny4860@gmail.com>
2018-12-27 17:55:12 +0900

    button 객체의 event listener 를 연결

--- 2-Intent/app/src/main/java/com/github/myapplication/MainActivity.java
+++ 2-Intent/app/src/main/java/com/github/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,10 +14,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // res/layout/activity_main.xml file 에 정의되어있는
         // widget/layout 정보와 mapping
         setContentView(R.layout.activity_main);
+
+        //btn_call 과 onClick() 을 연결
+        Button button = (Button) findViewById(R.id.btn_call);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-
+        
     }
 }
