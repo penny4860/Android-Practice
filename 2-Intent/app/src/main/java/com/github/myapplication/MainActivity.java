1de0a2f377bc961e8502be2a34e7527b3fe05451
jeongjoonsup <penny4860@gmail.com>
2018-12-27 18:25:19 +0900

    새로운 empty activity 추가

--- 2-Intent/app/src/main/java/com/github/myapplication/MainActivity.java
+++ 2-Intent/app/src/main/java/com/github/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_DIAL);
+        startActivity(intent);
         
     }
 }
