587fb5ab85bc53558bba3998d3809dda6fd4d84d
jeongjoonsup <penny4860@gmail.com>
2018-12-27 21:13:35 +0900

    onClick 콜백함수 구현

--- 3-View/app/src/main/java/com/github/a3_view/MainActivity.java
+++ 3-View/app/src/main/java/com/github/a3_view/MainActivity.java
@@ -2,12 +2,21 @@ package com.github.a3_view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.iv_like).setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
     }
 }
