6713832c3243a9b351a1560f11eb92c95cd7b3f9
jeongjoonsup <penny4860@gmail.com>
2018-12-28 17:48:11 +0900

    뷰 클릭 이벤트 구현

--- 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/LeftFragment.java
+++ 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/LeftFragment.java
@@ -1,6 +1,7 @@
 package com.example.penny.a4_fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,12 +14,23 @@ import android.view.ViewGroup;
  */
 public class LeftFragment extends Fragment implements View.OnClickListener {
 
+    // 1. event listener interface 를 private 변수로 정의
+    private OnColorButtonListener onColorButtonListener;
 
     public LeftFragment() {
         // Required empty public constructor
     }
 
+    // 2. onColorButtonListener(private 변수) 의 타입을 정의 : MainActivity class를 type casting
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        onColorButtonListener = (OnColorButtonListener) context;
+    }
 
+    // 3. Fragment가 가지고 있는 View 객체의 id 별로 onclick listener 함수를 mapping
+    // view.findViewById(R.id.v_blue).setOnClickListener(this);
+    // v_blue를 LeftFragment.onClick() 에 mapping
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,8 +42,25 @@ public class LeftFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+    // 4. View 객체를 클릭했을 때 실행되는 listener
+    // MainActivity class method가 실행
     @Override
     public void onClick(View v) {
+
+        switch (v.getId())
+        {
+            case R.id.v_red:
+                // onColorButtonListener : MainActivity class (2번과정)
+                onColorButtonListener.onColorClick(0);
+                break;
+            case R.id.v_green:
+                onColorButtonListener.onColorClick(1);
+                break;
+            case R.id.v_blue:
+                onColorButtonListener.onColorClick(2);
+                break;
+
+        }
 
     }
 }
--- 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/MainActivity.java
+++ 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.penny.a4_fragment;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -18,6 +19,24 @@ public class MainActivity extends AppCompatActivity implements OnColorButtonList
     // 0: red 1: green 2: blue
     @Override
     public void onColorClick(int color) {
+
+        Fragment fragment;
+
+        switch (color)
+        {
+            case 0:
+                fragment = new RedFragment();
+                break;
+            case 1:
+                fragment = new GreenFragment();
+                break;
+            case 2:
+                fragment = new BlueFragment();
+                break;
+            default:
+                fragment = new BlueFragment();
+        }
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_fragment, fragment).commit();
 
     }
 }
