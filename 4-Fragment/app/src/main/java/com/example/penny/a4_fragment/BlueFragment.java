a0ad1b1e9ff0eaa40a5466fd5a72325ce6a789b6
jeongjoonsup <penny4860@gmail.com>
2018-12-28 15:26:17 +0900

    bugfix : container -> null

--- 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/BlueFragment.java
+++ 4-Fragment/app/src/main/java/com/example/penny/a4_fragment/BlueFragment.java
@@ -13,6 +13,6 @@ public class BlueFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blue, container);
+        return inflater.inflate(R.layout.fragment_blue, null);
     }
 }
