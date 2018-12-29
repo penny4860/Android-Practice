c946f74d11c098c763720dec5b7e6d1dd2dc07b2
jeongjoonsup <penny4860@gmail.com>
2018-12-29 12:45:59 +0900

    commnts

--- 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/MainActivity.java
+++ 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/MainActivity.java
@@ -14,16 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 1. ViewPager 객체 생성
         ViewPager viewPager = (ViewPager) findViewById(R.id.vp_pager);
+
+        // 2. Fragment 객체 생성
         Fragment[] arrFragments = new Fragment[3];
         arrFragments[0] = new RedFragment();
         arrFragments[1] = new YelloFragment();
         arrFragments[2] = new GreenFragment();
 
-        MyPagerAdaptor adaptor = new MyPagerAdaptor(getSupportFragmentManager(), arrFragments);
+        // 3. Adaptor 객체 생성
+        //  Fragment 객체를 ViewPager 객체에 전달하는 역할
+        MyPagerAdaptor adaptor = new MyPagerAdaptor(getSupportFragmentManager(), arrFragments);.
         viewPager.setAdapter(adaptor);
     }
 
+    // Fragment 객체를 Pager 에 전달하기 위한 Adaptor
     private class MyPagerAdaptor extends FragmentPagerAdapter {
 
         private Fragment[] arrFragments;
