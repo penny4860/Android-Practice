8addeccb609929dc6cc5e3ba9bac26b7ab3a4e2c
jeongjoonsup <penny4860@gmail.com>
2018-12-29 12:05:43 +0900

    fragment added

--- 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/YelloFragment.java
+++ 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/YelloFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BLueFragment extends Fragment {
+public class YelloFragment extends Fragment {
 
 
-    public BLueFragment() {
+    public YelloFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class BLueFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        return inflater.inflate(R.layout.fragment_yello, container, false);
     }
 
 }
