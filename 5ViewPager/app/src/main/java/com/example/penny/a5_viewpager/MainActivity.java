9c30198c8c49319d71eeb1d6d3a67fee9ed1fd8d
jeongjoonsup <penny4860@gmail.com>
2018-12-29 12:40:10 +0900

    viewPager 에 fragment를 넣는 기능 구현

--- 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/MainActivity.java
+++ 5ViewPager/app/src/main/java/com/example/penny/a5_viewpager/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.penny.a5_viewpager;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_pager);
+        Fragment[] arrFragments = new Fragment[3];
+        arrFragments[0] = new RedFragment();
+        arrFragments[1] = new YelloFragment();
+        arrFragments[2] = new GreenFragment();
+
+        MyPagerAdaptor adaptor = new MyPagerAdaptor(getSupportFragmentManager(), arrFragments);
+        viewPager.setAdapter(adaptor);
+    }
+
+    private class MyPagerAdaptor extends FragmentPagerAdapter {
+
+        private Fragment[] arrFragments;
+
+        public MyPagerAdaptor(FragmentManager fm, Fragment[] arrFragments) {
+            super(fm);
+            this.arrFragments = arrFragments;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return arrFragments[i];
+        }
+
+        @Override
+        public int getCount() {
+            return arrFragments.length;
+        }
     }
 }
