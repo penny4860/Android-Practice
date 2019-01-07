59d682fef9afed078cb6a635964bd442bc44302e
jeongjoonsup <penny4860@gmail.com>
2019-01-07 21:21:20 +0900

    textureView.setSurfaceTextureListener 구현 중

--- 10-Camera2API/app/src/main/java/com/example/penny/a10_camera2api/MainActivity.java
+++ 10-Camera2API/app/src/main/java/com/example/penny/a10_camera2api/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.penny.a10_camera2api;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textureView = (TextureView) findViewById(R.id.textureView);
+
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener(){
+
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
     }
 }
