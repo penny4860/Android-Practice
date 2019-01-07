c17cf31aca86cb5f70d4abdf8035a474222da05b
jeongjoonsup <penny4860@gmail.com>
2019-01-07 21:47:25 +0900

    listener를 익명 클래스로 추가

--- 10-Camera2API/app/src/main/java/com/example/penny/a10_camera2api/MainActivity.java
+++ 10-Camera2API/app/src/main/java/com/example/penny/a10_camera2api/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         textureView = (TextureView) findViewById(R.id.textureView);
 
+        // SurfaceTextureListener listener : 익명 class -> interface를 구현하면서 instance를 생성
         textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener(){
 
             @Override
             public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-
+                openCamera();
             }
 
             @Override
@@ -40,5 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnCapture = (Button) findViewById(R.id.btnCapture);
+        btnCapture.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
     }
+
+    private void openCamera()
+    {
+
+    }
+
+    private void takePicture()
+    {
+
+    }
+
 }
